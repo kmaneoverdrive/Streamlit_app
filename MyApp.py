54ec786478ffef1d19cb3e12f1bb2a064cb46f10
kmaneoverdrive <86678605+kmaneoverdrive@users.noreply.github.com>
2021-08-11 12:37:56 +0200

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -4,14 +4,17 @@ Spyder Editor
 
 This is a temporary script file.
 """
-
+# Imports
 import streamlit as st
 
+# Hello world attempt
 st.write("""
          # My first app
          Hello *World!*
          """)
 
+# Getting the user name
 user_input = st.text_input("Please enter your username", nameless)
 
+# Saying hello to the user
 st.write("Hello " + user_input + "!")
