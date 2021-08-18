32d23b7d034532a1c33c58cfa7efe9adbd55ef93
kmaneoverdrive <86678605+kmaneoverdrive@users.noreply.github.com>
2021-08-19 00:14:46 +0200

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -38,6 +38,3 @@ df = pd.read_excel("Data/ahrefs_klint.xlsx")
 
 st.dataframe(df)
 
-df = pd.DataFrame(np.random.randn(50, 20),columns=('col %d' % i for i in range(20)))
-st.dataframe(df)  # Same as st.write(df)
-
