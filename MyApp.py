fc7979d51afb0d9af45d0c192f52a9de061046d9
kmaneoverdrive <86678605+kmaneoverdrive@users.noreply.github.com>
2021-08-19 12:57:37 +0200

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -23,17 +23,7 @@ user_input = st.text_input("Please enter your username", "nameless")
 
 # Saying hello to the user
 st.write("Hello " + user_input + "!")
-
-# File download experiment
-def get_table_download_link(df):
-    """Generates a link allowing the data in a given panda dataframe to be downloaded
-    in:  dataframe
-    out: href string
-    """
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-    href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
          
 df = pd.read_excel("Data/ahrefs_klint.xlsx")
 
-get_table_download_link(df)
+
